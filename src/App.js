91945a134d03d15f7a08840fd29c30bb8e79029e
Himanshu <himanshu@subzcribe.in>
2021-06-22 13:57:20 -0700

    Routing Added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import './App.css';
 import React from 'react';
-import { Button } from '@material-ui/core';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import SignUp from './pages/signup';
 import SignIn from './pages/signin';
 
 function App() {
   return (
-    //<SignUp/>
-    <SignIn/>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/signin">Sign in</Link>
+            </li>
+            <li>
+              <Link to="/signup">Sign up</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/signin">
+            <SignIn/>
+          </Route>
+          <Route exact path="/signup">
+            <SignUp/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
