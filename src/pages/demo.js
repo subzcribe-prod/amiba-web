51e4af72801de4af6bd2ef4261d3d6445c7d1c59
sankettchauhan <sanket.chauhan4@gmail.com>
2021-10-25 14:02:25 +0530

    error handling added to demo

--- src/pages/demo.js
+++ src/pages/demo.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { getApiResponse } from "../axios/demo";
 import CustomTable from "../components/demo/CustomTable";
-import { Typography } from "@material-ui/core";
+import { Typography, Paper } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -10,8 +10,7 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
+    color: "red",
   },
   json: {
     backgroundColor: "#DCDCDC",
@@ -23,15 +22,18 @@ export default function Demo({ history }) {
   const classes = useStyles();
   const [response, setResponse] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(false);
 
   async function load() {
     try {
       const res = await getApiResponse();
-      setResponse(JSON.parse(res.data));
+      if (!Array.isArray(res?.data))
+        return setError("Bad Request. The response JSON should be an array.");
+      if (res.data.length <= 0) return setError("No data available..");
+      setResponse(res.data);
     } catch (error) {
       console.log("err", error.response);
-      setError(error.response);
+      setError("Something went wrong..");
     } finally {
       setLoading(false);
     }
@@ -43,43 +45,15 @@ export default function Demo({ history }) {
 
   if (loading) return <h1>Loading..</h1>;
 
-  if (error && Object.keys(error).length > 0) {
-    let errorResponse, errorResponseKeys;
-    try {
-      errorResponse = error.data ? JSON.parse(error.data) : null;
-      errorResponseKeys = errorResponse ? Object.keys(errorResponse) : null;
-    } catch (err) {
-      errorResponse = "Please enter a valid JSON.";
-    }
-
+  if (error) {
     return (
       <>
-        <Typography variant="h4">
-          An error occured. Status code{" "}
-          <span style={{ fontWeight: "bold" }}>{error.status}</span>
-        </Typography>
-        <Typography variant="h4">
-          Response from server:
-          {errorResponseKeys ? (
-            <div className={classes.json}>
-              &#123;
-              {errorResponseKeys &&
-                errorResponseKeys.map((key, index) => (
-                  <div>
-                    {key}:{errorResponse[key]}
-                  </div>
-                ))}
-              &#125;
-            </div>
-          ) : (
-            <div>{errorResponse}</div>
-          )}
-        </Typography>
+        <Paper className={classes.paper}>
+          <Typography variant="h4">{error}</Typography>
+        </Paper>
       </>
     );
   }
-
-  if (response === null) return <h1>No response.</h1>;
 
   return (
     <>
